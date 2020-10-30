1a6f6514df37b2494f596763e830faaf2ed908e1
Yan Buinevich <iyanbuinevich799@gmail.com>
2020-10-30 11:28:37 +0300

    updated main function

--- lr5.cpp
+++ lr5.cpp
@@ -1,1 +1,20 @@
-﻿
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <locale.h>
+
+char** mGen(int n, int m);
+void check(char** str, int m, int n);
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int n; // количество строк
+	int m; // количество символов в строке
+	printf("Введите порядок матрицы: ");
+	scanf_s("%d", &n);
+	printf("\nВведите количество символов в строке: ");
+	scanf_s("%d", &m);
+	mGen(n, m);
+}
